b8cccc6264276d0b50ee10bdec93bc6b41b543b0
Brian Hsiang <107973296+brianh-osu@users.noreply.github.com>
2024-01-04 10:59:02 -0800

    Update tic-tac-toe (3).py
    validation and play are now modular

--- tic-tac-toe (3).py
+++ tic-tac-toe (3).py
@@ -19,115 +19,47 @@ board = [['_', '_', '_'],['_', '_', '_'],['_', '_', '_']]
 print('The board has been initialized as:')
 print(board)
 
-
-def check_win_conditions(letter, player_num):
-    """Pass in letter which is a string of the player's piece 
-    checks if win conditions exist, if it does return True otherwise return False"""
-    def horizontal_win():
-        for row in board: 
-            if row.count(letter) == len(board[0]): 
-                #print('Horizontal win')
-                return True 
-        return False 
-            
-    #vertical win 
-    def vertical_win():
-        #initialize count array and iterate through board row, cols 
-        count_arr = [0 for x in board]
-        for r in range(len(board[0])):
-            for c in range(len(board[0])):
-                row = board[r][c]
-                for l in row:
-                    if l == letter: 
-                        count_arr[c] += 1 
-            if count_arr.count(0) == 4: #if there is no counts in first row, no vertical win condition
-                return False 
-                
-        #check the indicies within count_arr. If any sum up correct then it is a vertical win 
-        for x in count_arr:
-            if x == len(board[0]):
-                #print('Vertical win')
-                return True 
-        return False 
-    
-    #diaganol win 
-    def diaganol_win():
-        count = 0 
-        if board[0][0] == letter:
-            count = 1 
-            #while board[cur][cur] == letter: 
-            for x in range(1, len(board[0])):
-                if board[x][x] == letter: 
-                    count += 1 
-        if count == len(board[0]):
-            return True 
-        
-        count = 0 
-        row = 0 
-        if board[row][len(board[0])-1] == letter:
-            count = 1 
-            cur = len(board[0])-1  
-            #while board[cur][cur]: 
-            for col in range(cur-1,-1,-1):
-                row += 1 
-                if board[row][col] == letter: 
-                    count += 1 
-                    cur -= 1 
-        if count == len(board[0]):
-            return True 
-        
-        return False 
-    
-    win_satisfied = [horizontal_win(), vertical_win(), diaganol_win()]
-    if win_satisfied[0] is True:
-        print('You have won by horizontal, player:', player_num)
-        return True 
-    elif win_satisfied[1] is True:
-        print('You have won by vertical, player:', player_num)
-        return True 
-    elif win_satisfied[2] is True:
-        print('You have won by diaganol, player:', player_num)
-        return True 
-    if win_satisfied.count(False) == 3:
-        # print('Continue playing..')
-        return False 
-
-def input_validation(choice, player_num):
-    while choice == 0 or choice > (board_len**2):
+def input_validation(player_num):
+    choice = 0 
+    while choice <= 0 or choice > (board_len**2):
         choice = int(input(f"Player {player_num}, where would you like to place (#1-{total_spots})"))
-    return choice 
+        if choice <= 0 or choice > (board_len**2):
+            print('Invalid range (placement should be between 1 and '+ str(total_spots)+'. Try again.')
+        else: 
+            tile = get_tile(choice) #checking tile 
+            tile_r, tile_c = tile[0], tile[1] 
+            if board[tile_r][tile_c] != '_':
+            # while board[tile_r][tile_c] != '_':
+                # choice = prompt_choice(player_num)
+                print('Placement is used, try again.')
+                spot = input_validation(player_num)
+                return spot 
+    # print(tile)
+    return tile 
     
-total_spots = len(board[0])**2
-while True: 
-    #prompt play 
-    board_len = len(board[0])
-    p1_choice = input_validation(0, 1)
-    p2_choice = input_validation(0, 2)
+def get_tile(choice):
+    """converts choice to tile# (returns an array)"""
+    row = choice // board_len 
+    col = (choice % board_len) - 1 
+    if col == -1:
+        row = int(choice / board_len) - 1 
+        col = board_len - 1 
+    return [row, col] 
     
-    #calculate positions to place on board 
-    p1_r = p1_choice // board_len 
-    p1_c = (p1_choice % board_len) - 1 
-    if p1_c == -1: 
-        p1_r = int(p1_choice / board_len)-1
-        p1_c = board_len -1
-
-    p2_r = p2_choice // board_len 
-    p2_c = (p2_choice % board_len) - 1 
-    if p2_c == -1: 
-        p2_r = int(p2_choice / board_len)-1
-        p2_c = board_len-1
-
-    #place on board 
-    board[p1_r][p1_c] = 'O' 
-    board[p2_r][p2_c] = 'X'
-    
-    #print results 
+def play_tile(player_num, tile):
+    tile_r, tile_c = tile[0], tile[1]
+    if player_num == 1:
+        board[tile_r][tile_c] = 'O'
+    elif player_num == 2:
+        board[tile_r][tile_c] = 'X' 
+    print('The board is now: ')
     print(board)
-    if check_win_conditions('O', 1 ) is True:
-        print('The game is over, you have won player 1.')
-        break 
-    elif check_win_conditions('X', 2) is True:
-        print('The game is over, you have won player 2.')
-        break
-    print() #blank line
     
+board_len = len(board[0])
+total_spots = len(board[0])**2 
+
+p1_tile = input_validation(1)
+play_tile(1, p1_tile)
+p2_tile = input_validation(2)
+play_tile(2, p2_tile) 
+
