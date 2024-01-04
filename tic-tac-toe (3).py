6c7361733434520c4df770557bc1fbf2d54f7962
Brian Hsiang <107973296+brianh-osu@users.noreply.github.com>
2024-01-04 11:12:47 -0800

    Game play working, updated to do
    handle case where game is tied (all tilles used, no plays remain)

--- tic-tac-toe (3).py
+++ tic-tac-toe (3).py
@@ -1,21 +1,9 @@
 #TODO: Add GUI 
-#TODO: Data validation for input prompts ( 2. user cannot play on a used tile)
-#TODO: Game should end if win condition is met, and prompt user to play again or exit. 
 #TODO: If game is a draw (all tiles used, no plays remain) then game must break.
 
-board = [['O' for x in range(3)] for x in range(3)]
-board = [['O','O','O'],['X','X','X'],['X','X','X']]
-board = [['X','X','X'],['O','O','O'],['X','X','X']]
-board = [['O','X','O'], ['O', 'O', 'X'], ['O', 'O', 'X']]
-board = [['O','X','O'], ['X','O','X'],['O','O','O']]
-board = [['O','X','O'], ['X','O','X'],['X','X','X']]
-board = [['X', 'O', 'O'], ['O', 'X', 'O'], ['O', 'O', 'X']]
-board = [['O','O','X'], ['O', 'X', 'O'], ['X', 'O', 'O']]
-board = [['O','O','O'], ['O','O','O'], ['O','O','O']]
-board = [['_', '_', '_'],['_', '_', '_'],['_', '_', '_']]
-board = [['O', 'X', '_'], ['_', 'O', 'X'],['_', '_', '_']]
-board = [['_', '_', '_'],['_', '_', '_'],['_', '_', '_']]
-#board = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']]
+board = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']]
+# board = [['_','_','O'], ['O','O','O'], ['O','O','O']]
+# board = [['_', '_', 'X'], ['_', 'X', 'X'],['X', 'X', '_']]
 print('The board has been initialized as:')
 print(board)
 
@@ -55,11 +43,91 @@ def play_tile(player_num, tile):
     print('The board is now: ')
     print(board)
     
+def check_win_conditions(letter, player_num):
+    """Pass in letter which is a string of the player's piece 
+    checks if win conditions exist, if it does return True otherwise return False"""
+    def horizontal_win():
+        for row in board: 
+            if row.count(letter) == len(board[0]): 
+                #print('Horizontal win')
+                return True 
+        return False 
+            
+    #vertical win 
+    def vertical_win():
+        #initialize count array and iterate through board row, cols 
+        count_arr = [0 for x in board]
+        for r in range(len(board[0])):
+            for c in range(len(board[0])):
+                row = board[r][c]
+                for l in row:
+                    if l == letter: 
+                        count_arr[c] += 1 
+            if count_arr.count(0) == 4: #if there is no counts in first row, no vertical win condition
+                return False 
+                
+        #check the indicies within count_arr. If any sum up correct then it is a vertical win 
+        for x in count_arr:
+            if x == len(board[0]):
+                #print('Vertical win')
+                return True 
+        return False 
+    
+    #diaganol win 
+    def diaganol_win():
+        count = 0 
+        if board[0][0] == letter:
+            count = 1 
+            #while board[cur][cur] == letter: 
+            for x in range(1, len(board[0])):
+                if board[x][x] == letter: 
+                    count += 1 
+        if count == len(board[0]):
+            return True 
+        
+        count = 0 
+        row = 0 
+        if board[row][len(board[0])-1] == letter:
+            count = 1 
+            cur = len(board[0])-1  
+            #while board[cur][cur]: 
+            for col in range(cur-1,-1,-1):
+                row += 1 
+                if board[row][col] == letter: 
+                    count += 1 
+                    cur -= 1 
+        if count == len(board[0]):
+            return True 
+        
+        return False 
+        
+    win_satisfied = [horizontal_win(), vertical_win(), diaganol_win()]
+    if win_satisfied[0] is True:
+        print('You have won by horizontal, player:', player_num)
+        # return True 
+    if win_satisfied[1] is True:
+        print('You have won by vertical, player:', player_num)
+        # return True 
+    if win_satisfied[2] is True:
+        print('You have won by diaganol, player:', player_num)
+        # return True 
+    if win_satisfied.count(False) == 3:
+        # print('Continue playing..')
+        return False 
+    else:
+        return True 
+    
 board_len = len(board[0])
 total_spots = len(board[0])**2 
 
-p1_tile = input_validation(1)
-play_tile(1, p1_tile)
-p2_tile = input_validation(2)
-play_tile(2, p2_tile) 
-
+while True:
+    p1_tile = input_validation(1)
+    play_tile(1, p1_tile)
+    p2_tile = input_validation(2)
+    play_tile(2, p2_tile) 
+    if check_win_conditions('O', 1) is True:
+        print('The game is over, you have won player 1.')
+        break 
+    elif check_win_conditions('X', 2) is True:
+        print('The game is over, you have won player 2.')
+        break 
